5bf64ffcc2240f75d170b7964c6ee72fd66b99f2
Kleo Kapo <kapokleo07@gmail.com>
2025-06-27 17:10:22 +0200

    Aggiunto file html per la leaderBoard, aggiungo una loading screen metendo la display di tutti gli elementi trane lo sfondo a "display: none;" e la display della loading element di aparirsi

--- untitled3/code/js/home.js
+++ untitled3/code/js/home.js
@@ -1,4 +1,5 @@
 function change_class_bodyCont(){
     document.getElementById("rimuovere").classList.remove("bodyCont");
     document.getElementById("rimuovere").classList.add("displayNone");
+    document.getElementById("aggiungere").style.display = "flex";
 }
