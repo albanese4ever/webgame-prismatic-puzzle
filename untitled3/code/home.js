bb4bbafdb622c414b6a48da0497da2c06cf51dd1
ITSINCOMEV11 <rekshuyangyang@gmail.com>
2025-07-04 10:07:59 +0200

    fix

--- untitled3/code/home.js
+++ untitled3/code/home.js
@@ -1,6 +1,49 @@
 const options = document.querySelectorAll('.difficulty-option');
 const bg      = document.querySelector('.selector-background');
 
+if (document.readyState === 'complete') {
+    if(localStorage.getItem('token')=== null){
+        document.getElementById("logout").innerHTML="Login";
+    }else{
+        document.getElementById("logout").innerHTML="Logout";
+    }
+} else {
+    window.addEventListener('load', () => {
+        if(localStorage.getItem('token')=== null){
+            document.getElementById("logout").innerHTML="Login";
+        }else{
+            document.getElementById("logout").innerHTML="Logout";
+        }
+    });
+}
+let difficulty;
+function multiplayerclick(){
+    localStorage.setItem("multi","1");
+    localStorage.setItem("difficulty", "2");
+    window.location.href='partita.html'
+
+}
+function singleplayerclick(){
+    localStorage.setItem("multi","0");
+    localStorage.setItem("difficulty", difficulty);
+    window.location.href='index.html'
+}
+function logincheck(){
+    if(localStorage.getItem("token") === null){
+        window.location.href="login.html";
+    }else{
+        alert("Sei gia loggato ad un profilo.")
+    }
+}
+function logout() {
+    if (localStorage.getItem("token") !== null) {
+        localStorage.removeItem("token");
+        alert("Logout effettuato")
+        window.location.href = "home.html";
+    }
+    window.location.href = "login.html";
+}
+
 function moveBackgroundTo(el) {
     const w = el.offsetWidth, h = el.offsetHeight;
     const x = el.offsetLeft,  y = el.offsetTop;
@@ -8,6 +51,20 @@ function moveBackgroundTo(el) {
     bg.style.height = h + 'px';
     bg.style.left   = x + 'px';
     bg.style.top    = y + 'px';
+    const activeElement = document.querySelector('.difficulty-option.active');
+    const dataValue = activeElement.getAttribute('data-value');
+    console.log(dataValue);
+    switch (dataValue) {
+        case 'easy':
+            difficulty = 1;
+            break;
+        case 'medium':
+            difficulty = 2;
+            break;
+        case 'hard':
+            difficulty = 3;
+            break;
+    }
 }
 
 // init
